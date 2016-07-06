2b7cf2d67e5d1f5e8505022f61ca7d46f37330d1
hcz <h.cz@qq.com>
2016-07-06 22:01:48 +0800

    add files

---
+++ sos.client.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var domain = require('domain');
+var socks = require('socksv5');
+
+process.on('uncaughtException', function (err) {
+    console.log(err.stack);
+});
+
+var server = socks.createServer(function (info, accept, deny) {
+    if (
+        info.srcAddr !== '127.0.0.1'
+        && info.srcAddr !== '::ffff:127.0.0.1'
+        && info.srcAddr !== '::1'
+    ) {
+        console.log('from ' + info.srcAddr + ':' + info.srcPort);
+    }
+    console.log('open ' + info.dstAddr + ':' + info.dstPort);
+
+    var d = domain.create();
+
+    d.on('error', function (err) {
+        console.log(err.stack);
+        d.dispose();
+    });
+
+    d.run(function () {
+        var wsocket = new ws(process.argv[3]);
+
+        wsocket.onopen = function () {
+            wsocket.send(JSON.stringify({
+                host: info.dstAddr,
+                port: info.dstPort,
+            }), {binary: false});
+
+            var nsocket = accept(true);
+
+            nsocket.on('data', function (data) {
+                wsocket.send(data, {binary: true, compress: true});
+            });
+
+            nsocket.on('end', function () {
+                wsocket.close();
+            });
+
+            wsocket.onmessage = function (message) {
+                nsocket.write(message.data);
+            };
+
+            wsocket.onclose = function () {
+                nsocket.end();
+            };
+        };
+    });
+});
+
+server.useAuth(socks.auth.None());
+
+server.listen(parseInt(process.argv[2], 10));
---
+++ sos.server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var domain = require('domain');
+var net = require('net');
+var ws = require('ws');
+
+process.on('uncaughtException', function (err) {
+    console.log(err.stack);
+});
+
+var server = ws.Server({
+    port: parseInt(process.argv[2], 10)
+});
+
+server.on('connection', function (wsocket) {
+    // console.log('from ' + wsocket.???.remoteAddress);
+
+    var d = domain.create();
+
+    d.add(wsocket);
+    d.on('error', function (err) {
+        console.log(err.stack);
+        d.dispose();
+    });
+
+    d.run(function () {
+        var nsocket = new net.Socket();
+
+        wsocket.onmessage = function (message) {
+            var info = JSON.parse(message.data);
+
+            console.log('open ' + info.host + ':' + info.port);
+
+            nsocket.connect({
+                host: info.host,
+                port: info.port,
+            });
+
+            nsocket.on('data', function (data) {
+                wsocket.send(data, {binary: true, compress: true});
+            });
+
+            nsocket.on('end', function () {
+                wsocket.close();
+            });
+
+            wsocket.onmessage = function (message) {
+                nsocket.write(message.data);
+            };
+
+            wsocket.onclose = function () {
+                nsocket.end();
+            };
+        };
+    });
+});
